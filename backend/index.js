9f217d26c942c4f3e3e06ca71bd73ac4fa80d02f
Vivek Namdev <viveknamdev257@gmail.com>
2024-11-26 17:23:18 +0530

    added structure of the project

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+
+//middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true // Note: 'Credentials' should be 'credentials'
+};
+
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> {
+    connectDB();
+    console.log(`Server is running at port ${PORT}`);
+})
