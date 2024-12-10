fce06cccf95116524f36e628a0e5607ce25e8e01
Vivek Namdev <viveknamdev257@gmail.com>
2024-12-10 22:41:10 +0530

    End Game

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -3,37 +3,37 @@ import { Job } from "../models/job.model.js";
 
 export const applyJob = async (req, res) => {
     try {
-        const userId = req.body;
+        const userId = req.id;
         const jobId = req.params.id;
-
-        if(!jobId) {
+        if (!jobId) {
             return res.status(400).json({
-                message: "Job id isrequired.",
+                message: "Job id is required.",
                 success: false
             })
-        }
+        };
+        // check if the user has already applied for the job
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId });
 
-        const existingApplication = await application.findOne({job: jobId, applicant: userId});
-
-        if(existingApplication) {
+        if (existingApplication) {
             return res.status(400).json({
                 message: "You have already applied for this jobs",
                 success: false
-            })
+            });
         }
 
+        // check if the jobs exists
         const job = await Job.findById(jobId);
-        if(!job) {
+        if (!job) {
             return res.status(404).json({
                 message: "Job not found",
                 success: false
             })
         }
-
+        // create a new application
         const newApplication = await Application.create({
-            Job: jobId,
-            applicant: userId,
-        })
+            job:jobId,
+            applicant:userId,
+        });
 
         job.applications.push(newApplication._id);
         await job.save();
@@ -41,32 +41,27 @@ export const applyJob = async (req, res) => {
             message:"Job applied successfully.",
             success:true
         })
-
     } catch (error) {
         console.log(error);
     }
-}
-
-
-export const getAppliedJobs = async (req, res) => {
+};
+export const getAppliedJobs = async (req,res) => {
     try {
         const userId = req.id;
-        const application = await Application.find({application: userId}).sort({createdAt: -1}).populate({
-            path: 'job',
-            options: {sort: {createdAt: -1}},
+        const application = await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:'job',
+            options:{sort:{createdAt:-1}},
             populate:{
-                path: 'company',
-                options: {sort: {createdAt: -1}},
+                path:'company',
+                options:{sort:{createdAt:-1}},
             }
-        })
-
-        if(!application) {
+        });
+        if(!application){
             return res.status(404).json({
                 message:"No Applications",
                 success:false
             })
-        }
-
+        };
         return res.status(200).json({
             application,
             success:true
@@ -75,56 +70,52 @@ export const getAppliedJobs = async (req, res) => {
         console.log(error);
     }
 }
-
-
-export const getApplicants = async (req, res) => {
+// admin dekhega kitna user ne apply kiya hai
+export const getApplicants = async (req,res) => {
     try {
         const jobId = req.params.id;
-        const job = await job.findById(jobId).populate({
-            path: 'applications',
-            options:{sort:{createdAt: -1}},
-            populate: {
-                path: 'applicant'
+        const job = await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
             }
-        })
-
-        if(!job) {
+        });
+        if(!job){
             return res.status(404).json({
                 message:'Job not found.',
                 success:false
             })
-        }
-
+        };
         return res.status(200).json({
             job, 
             succees:true
         });
-
     } catch (error) {
         console.log(error);
     }
 }
-
-
-export const updateStatus = async (req, res) => {
+export const updateStatus = async (req,res) => {
     try {
         const {status} = req.body;
         const applicationId = req.params.id;
-        if(!status) {
+        if(!status){
             return res.status(400).json({
                 message:'status is required',
                 success:false
             })
-        }
+        };
 
-        const application = await Application.findOne({_id: applicationId});
-        if(!application) {
+        // find the application by applicantion id
+        const application = await Application.findOne({_id:applicationId});
+        if(!application){
             return res.status(404).json({
                 message:"Application not found.",
                 success:false
             })
-        }
+        };
 
+        // update the status
         application.status = status.toLowerCase();
         await application.save();
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,38 +2,37 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     fullname: {
-        type:String,
+        type: String,
         required: true
     },
     email: {
-        type:String,
+        type: String,
         required: true,
         unique: true
     },
     phoneNumber: {
+        type: Number,
+        required: true
+    },
+    password:{
         type:String,
-        required: true
+        required:true,
     },
-    password: {
+    role:{
         type:String,
-        required: true
+        enum:['student','recruiter'],
+        required:true
     },
-    role: {
-        type: String,
-        enum: ['student', 'recruiter'],
-        required: true
+    profile:{
+        bio:{type:String},
+        skills:[{type:String}],
+        resume:{type:String}, // URL to resume file
+        resumeOriginalName:{type:String},
+        company:{type:mongoose.Schema.Types.ObjectId, ref:'Company'}, 
+        profilePhoto:{
+            type:String,
+            default:""
+        }
     },
-    profile: {
-        bio: {type: String},
-        skills: [{type: String}],
-        resume: {type: String},
-        resumeOriginalName: {type: String},
-        company: {type:mongoose.Schema.Types.ObjectId, ref: 'Company'},
-        profilePhoto: {
-            type: String,
-            default: ""
-        },
-    },
-}, {timestamps:true});
-
+},{timestamps:true});
 export const User = mongoose.model('User', userSchema);
